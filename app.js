d2e75b752343e99bda52ab387a5a135b0d4b999a
William Huang <shsidforever@gmail.com>
2018-10-05 00:23:38 +0800

    added download function

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+//wrapper for reddit api
 const snoowrap = require('snoowrap');
+
+//for downloading images
 const fs = require('fs');
+const Path = require('path');
+const axios = require('axios');
+
+//config file
 const config = require('./config.json')
-const regex = /\[[.,"!\w\d\s-]+\]\(https:\/\/(i.imgur|imgur).com\/[\w]+(\/[\w]+|.png|.jpg|.gifv)\)/g
+
+//regex
+const regex_full = /\[[.,"!\w\d\s-]+\]\(https:\/\/(i.imgur|imgur).com\/[\w]+(\/[\w]+|.png|.jpg|.gifv)\)/g
+const regex_comment = /[.,"!\w\d\s-]+/g
+const regex_link = /https:\/\/(i.imgur|imgur).com\/[\w]+(\/[\w]+|.png|.jpg|.gifv)/g
+const regex_file_name = /[\w]+(.png|.jpg|.gifv)/g
+
+//arrays
 let link_comments = []
+let mapping = []
 // Create a new snoowrap requester with OAuth credentials.
 // For more information on getting credentials, see here: https://github.com/not-an-aardvark/reddit-oauth-helper
 const r = new snoowrap({
@@ -30,12 +45,51 @@ async function main() {
     response.comments.forEach(function (comment) {
         //only care about comments with imgur links
         if (comment.body.includes('imgur.com')) {
-            let result = comment.body.match(regex)
+            let result = comment.body.match(regex_full)
             //store results into array
             if(Array.isArray(result)) link_comments = link_comments.concat(result)
             else link_comments.push(result)
         }
     })
-
+    split()
 }
+
+function split(){
+    link_comments.forEach((ref)=>{
+        let comment = ref.match(regex_comment)[0]
+        let link = ref.match(regex_link)[0]
+        //can't be bothered to deal with albums just yet
+        if(!link.includes('/a/')){
+            let filename = link.match(regex_file_name)[0]
+            //handle gifv cases
+            filename = filename.replace('.gifv','.mp4')
+            
+            download(link,filename)
+        }
+        //maybe add to database in the future
+        mapping.push({link:link,comment:comment})
+    })
+    // console.log(mapping)
+}
+
+//download
+async function download(link,filename){
+    const path = Path.resolve(__dirname,'./image',filename)
+    const response = await axios({
+        method: 'GET',
+        url: link,
+        responseType: 'stream'
+    })
+    response.data.pipe(fs.createWriteStream(path))
+    return new Promise((resolve,reject)=>{
+        response.data.on('end',()=>{
+            resolve()
+        })
+        response.data.on('error',err=>{
+            reject(err)
+        })
+    })
+       
+}
+
 main()
